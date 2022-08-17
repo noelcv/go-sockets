aa900bf5ceeba1b17aefed643d4ade216c119b50
noelcv <79803356+noelcv@users.noreply.github.com>
2022-08-17 11:39:11 +0200

    add: iport Gorilla packages and setup websockets

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
@@ -13,18 +14,48 @@ type Message struct {
 }
 
 var (
-	wsUpgrader = websocket.wsUpgrader {
+	wsUpgrader = websocket.Upgrader {
 		ReadBufferSize: 1024,
-		WriteBufferSize: 1024
+		WriteBufferSize: 1024,
 	}
 	
-	wsConn *websocket.Conn //pointer to the ws connection
+	wsConn *websocket.Conn 
+	//pointer to the ws connection
 )
+
+func WsEndpoint(w http.ResponseWriter, r *http.Request) {
+	
+	wsUpgrader.CheckOrigin = func(r *http.Request) bool {
+   //check the http request and make sure it's okay to access
+	return true
+	}
+	
+	wsConn, err := wsUpgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Printf("could not upgrade: %s\n", err.Error())
+		return
+	}
+	
+	defer wsConn.Close();
+	//event loop 
+	for {
+		var msg Message
+		err := wsConn.ReadJSON(&msg) 
+		if err != nil {
+			fmt.Printf("error reading JSON: %s\n", err.Error())
+			break
+		}
+		fmt.Printf("Message Received: %s\n", msg.Greeting)
+	}
+	
+}
 
 
 func main() {
 	
 	router := mux.NewRouter();
 	
-	log.Fatal(http.ListenAndServe(":9100", router))
+	router.HandleFunc("/socket", WsEndpoint)
+	
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
